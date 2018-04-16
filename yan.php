4fa97071c87a4d74f6f36f36fb0e78e68abc25a1
rrose98 <36145326+rrose98@users.noreply.github.com>
2018-04-16 16:36:44 +0800

    Update yan.php

--- yan.php
+++ yan.php
@@ -13,7 +13,7 @@ body{
 <body>
 <img src="yanpict.jpg" height="300" width="300">
 <h1><center>JOHN RYAN REMETICADO</h1></center>
-<p><a href="ako.html" target="content">HOME</a></p>
+<p><a href="ako.php" target="content">HOME</a></p>
 <h1><center>INFORMATION</h1></center>
 <p><front = 'Georgia'><b>First Name:</b>John Ryan</p>
 <p><front = 'Georgia'><b>Middle Name:</b>Alajid</p>
