f5c54ba4258a137908c4685a238370e69899972e
rrose98 <36145326+rrose98@users.noreply.github.com>
2018-04-16 16:34:06 +0800

    Update kuya.php

--- kuya.php
+++ kuya.php
@@ -13,7 +13,7 @@ body{
 <body>
 <img src="kuyapict.jpg" height="300" width="300">
 <h1><center>JOHN MARK REMETICADO</center></h1>
-<p><a href="ako.html" target="content">HOME</a></p>
+<p><a href="ako.php" target="content">HOME</a></p>
 <h1><center>INFORMATION</h1></center>
 <p><front = 'Georgia'><b>First Name:</b>John Mark</p>
 <p><front = 'Georgia'><b>Middle Name:</b>Alajid</p>
